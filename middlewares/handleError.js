c83302179e3928c16247c3b681243861556d50e7
TheMatius <thematius.monsalve@gmail.com>
2021-05-07 13:19:47 -0500

    Mejorando la funcion de los errores con un diccionario de funciones.

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,21 +1,22 @@
-const handleError = (error, req, res, next) => {
-  const { name, type } = error;
-
-  switch (name) {
-  case 'CastError':
-    return res.status(400).end();
-  case 'ValidatorError':
-    if (type == 'USER_UNIQUE') {
+const ERROR_HANDLERS = {
+  CastError: res => res.status(400).end(),
+  ValidatorError: (res, type) => {
+    if (type === 'USER_UNIQUE') {
       return res.status(500).json({
         ok: false,
         error: 'El nombre de usuario ya existe.'
       });
     }
     return res.status(500).end();
-  default:
-    res.status(500).end();
-  }
-  res.status(500).end();
+  },
+  JsonWebTokenError: res => res.status(401).end(),
+  DefaultError: res => res.status(500).end()
+};
+
+const handleError = (error, req, res, next) => {
+  const { name, type } = error;
+  const handler = ERROR_HANDLERS[name] || ERROR_HANDLERS.DefaultError;
+  handler(res, type);
 };
 
 module.exports = handleError;
